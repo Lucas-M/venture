76d5c76f37642990a956b8f874c25c2dafa016b8
Lucas-M <lucas.macd@gmail.com>
2016-05-02 12:59:53 -0400

    initial object creation

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
 
+function tester() {
+	console.log("clicked")
+	console.log(hello_clicker.display_text);
+
+}
+
+function test2() {
+	console.log(hello_clicker.sayHello());
+}
+
+var Clicker = function(display_text) {
+	this.display_text = display_text;
+	console.log("Clicker instance created");
+};
+
+Clicker.prototype.sayHello = function() {
+	console.log("Hello, I'm " + this.display_text);
+}
+
+
+var hello_clicker = new Clicker('Bob');
